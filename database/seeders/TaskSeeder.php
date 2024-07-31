5492de9e21aa43e6c91c82838b7ff4fd71356a6f
Precious O Atam <precious4prestige@gmail.com>
2024-07-31 18:35:22 +0100

    updated seeders and readme file

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TaskSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('tasks')->insert([
+            'title' => 'First Task',
+            'description' => 'A completed task',
+            'completed' => true,
+        ],);
+        DB::table('tasks')->insert([
+            'title' => 'Second Task',
+            'description' => 'An uncompleted task',
+            'completed' => false,
+        ],);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Middleware\AlwaysAcceptJson;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('tasks', TaskController::class);
+Route::middleware([AlwaysAcceptJson::class])->group(function () {
+    Route::resource('tasks', TaskController::class);
+});
